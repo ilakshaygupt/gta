369f053d7ea3eb0aa7c8c36cc92605a3c4661a50
Sarthak Rastogi <rsarthakverse123@gmail.com>
2023-10-07 22:24:46 +0530

    added collision blocks

--- js/canvas.js
+++ js/canvas.js
@@ -3,10 +3,58 @@ import Player from "./player.js"
 const canvas = document.querySelector('canvas')
 
 const context = canvas.getContext('2d')
+
 canvas.width = 1024
 canvas.height = 576
 
+//creating arrray to store collision points
+const collisionMap = []
+for (let i = 0; i < collisions.length; i = i + 100) {
+    collisionMap.push(collisions.slice(i, i + 100))
+}
 
+
+//creating the collision object we want to render
+class Boundary {
+    static width = 40
+    static height =40
+    constructor({ position }) {
+        this.position = position
+        this.width = 40  // 16 pixel * 250% zoom level = 4000
+        this.height = 40 // 16 pixel * 250% zoom level = 4000
+    }
+
+    //drawing the rectangles of collision
+    draw() {
+        context.fillStyle = 'red'
+        context.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const offset= {
+    x:0,
+    y:0
+  }
+
+// filling up Boundaries array with collison objects
+const Boundaries = [] 
+//iterating each row
+collisionMap.forEach((row , i) => {
+    //iterating inside each row
+    row.forEach((Symbol , j)=> {
+        if(Symbol=== 13188)
+        Boundaries.push(
+            new Boundary({
+                position :{
+                    x: j*Boundary.width +offset.x,  
+                    y: i*Boundary.height + offset.y
+                }
+            })
+        )
+    })
+})
+
+console.log(Boundaries)
 
 const image = new Image()
 image.src = "../assets/GTA_MAP[1]_updated.png"
@@ -16,10 +64,10 @@ let up = new Image();
 let down = new Image();
 let left = new Image();
 let right = new Image();
-up.src="../images/up/1.png";
-down.src="../images/down/1.png";
-left.src="../images/left/1.png";
-right.src="../images/right/1.png";
+up.src = "../images/up/1.png";
+down.src = "../images/down/1.png";
+left.src = "../images/left/1.png";
+right.src = "../images/right/1.png";
 const upImages = [];
 const downImages = [];
 const leftImages = [];
@@ -32,13 +80,13 @@ fillArray("right", 9, rightImages);
 
 
 function fillArray(folder, count, images) {
-  for (let i = 1; i <= count; i++) {
-    const image = new Image();
-    image.src = `./images/${folder}/${i}.png`;
-    images.push(image);
-  }
+    for (let i = 1; i <= count; i++) {
+        const image = new Image();
+        image.src = `./images/${folder}/${i}.png`;
+        images.push(image);
+    }
 }
-let player=new Player(512,270,69,69,upImages);
+let player = new Player(512, 270, 69, 69, upImages);
 
 class Sprite {
     constructor({ position, image }) {
@@ -50,36 +98,68 @@ class Sprite {
     }
 }
 
+
+
 image.onload = () => {
     const background = new Sprite({
         position: {
-            x: -1100,
-            y: -1100
+            x: offset.x,
+            y: offset.y
         },
         image: image
     })
+    
+   
 
     function animate() {
         context.clearRect(0, 0, canvas.width, canvas.height);
+        
+       
+       
+        
         background.draw();
+        
+
+        //drawing the boundaries 
+        Boundaries.forEach(Boundary => {
+            Boundary.draw()
+        })
+        
+        if (keys.w.pressed) {
+            player.setAnimation(upImages);
+            background.position.y += 2;
+            Boundaries.forEach(boundary => {
+                boundary.position.y+= 2;
+            });
+        } else if (keys.a.pressed) {
+            player.setAnimation(leftImages);
+            background.position.x += 2;
+            Boundaries.forEach(boundary => {
+                boundary.position.x += 2;
+            });
+        } else if (keys.s.pressed) {
+            player.setAnimation(downImages);
+            background.position.y -= 2;
+            Boundaries.forEach(boundary => {
+                boundary.position.y -= 2;
+            });
+        } else if (keys.d.pressed) {
+            player.setAnimation(rightImages);
+            background.position.x -= 2;
+            Boundaries.forEach(boundary => {
+                boundary.position.x -= 2;
+            });
+        }
+       
+       
+
+    
         player.updateAnimation();
         player.draw(context);
         window.requestAnimationFrame(animate);
 
 
-        if (keys.w.pressed) {
-            background.position.y += 2;
-            player.setAnimation(upImages);
-        } else if (keys.a.pressed) {
-            background.position.x += 2;
-            player.setAnimation(leftImages);
-        } else if (keys.s.pressed) {
-            background.position.y -= 2;
-            player.setAnimation(downImages);
-        } else if (keys.d.pressed) {
-            background.position.x -= 2;
-            player.setAnimation(rightImages);
-        }
+       
 
         // context.fillRect(512, 270, 50, 50);
 
@@ -145,5 +225,3 @@ window.addEventListener('keyup', (e) => {
     }
     console.log(keys)
 })
-
-
