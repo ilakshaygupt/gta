7c2be459641884102987ab7ddaa56fd781217473
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-10-08 22:59:19 +0530

    added car

--- js/canvas.js
+++ js/canvas.js
@@ -19,6 +19,10 @@ let upCounter=0;
 let downCounter=0;
 let leftCounter=0;
 let rightCounter=0;
+let upCarCounter=0;
+let downCarCounter=0;
+let leftCarCounter=0;
+let rightCarCounter=0;
 
 class Boundary {
     static width = 40
@@ -90,7 +94,6 @@ collisionMap.forEach((row, i) => {
 
 const image = new Image()
 image.src = '../assets/GTA_MAP[1]_updated.png'
-// console.log(image)
 const foregroundImage = new Image()
 foregroundImage.src = '../assets/Foreground.png'
 
@@ -101,21 +104,41 @@ let up = new Image();
 let down = new Image();
 let left = new Image();
 let right = new Image();
+let upCar = new Image();
+let downCar = new Image();
+let leftCar = new Image();
+let rightCar = new Image();
 up.src = "../images/up/1.png";
 down.src = "../images/down/1.png";
 left.src = "../images/left/1.png";
 right.src = "../images/right/1.png";
+upCar.src = "../car/upCar/1.png";
+downCar.src = "../car/downCar/1.png";
+leftCar.src = "../car/leftCar/1.png";
+rightCar.src = "../car/rightCar/1.png";
 const upImages = [];
 const downImages = [];
 const leftImages = [];
 const rightImages = [];
-
+const upCarImages = [];
+const downCarImages = [];
+const leftCarImages = [];
+const rightCarImages = [];
 fillArray("up", 6, upImages);
 fillArray("down", 9, downImages);
 fillArray("left", 9, leftImages);
 fillArray("right", 9, rightImages);
-
-
+fillCarArray("upCar", 2, upCarImages);
+fillCarArray("downCar", 2, downCarImages);
+fillCarArray("leftCar", 2, leftCarImages);
+fillCarArray("rightCar", 2, rightCarImages);
+function fillCarArray(folder, count, images) {
+    for (let i = 1; i <= count; i++) {
+        const image = new Image();
+        image.src = `./car/${folder}/${i}.png`;
+        images.push(image);
+    }
+}
 function fillArray(folder, count, images) {
     for (let i = 1; i <= count; i++) {
         const image = new Image();
@@ -124,6 +147,7 @@ function fillArray(folder, count, images) {
     }
 }
 let player = new Player(512, 270, 64, 64, upImages);
+let car = new Player(512, 270, 64, 64, upCarImages);
 class Sprite {
     constructor({ position, image }) {
         this.position = position
@@ -141,7 +165,9 @@ const testBoundary = new Boundary({
     }
 })
 let lastKey = 'w'
+let isCar = false;
 player.setAnimation(downImages.slice(0, 1));
+car.setAnimation(downCarImages.slice(0, 1));
 image.onload = () => {
     const background = new Sprite({
         position: {
@@ -170,22 +196,33 @@ image.onload = () => {
             rect1.x <= rect2.position.x + rect2.width - 20
             && rect1.y + rect1.height - 8 >= rect2.position.y &&
             rect1.y <= rect2.position.y + rect2.height - 50)
-    
     }
     function animate() {
+
         context.clearRect(0, 0, canvas.width, canvas.height);
         background.draw()
+        if(isCar){
+                upCounter=0;
+                leftCounter=0;
+                rightCounter=0;
+                downCounter=0;
+            car.draw(context)
+        }
+       else if(!isCar) {
+        upCarCounter=0;
+        leftCarCounter=0;
+        rightCarCounter=0;
+        downCarCounter=0;
         player.draw(context)
+       }
         foreground.draw()
-        // console.log('Foreground drwan')
-        //drawing the boundaries 
         Boundaries.forEach(Boundary => {
             Boundary.draw()
-
         })
         airplane.update();
         airplane.draw(context);
         let moving = true;
+       
         if (keys.w.pressed && lastKey === 'w') {
             for (let i = 0; i < Boundaries.length; i++) {
                 let curr = Boundaries[i];
@@ -197,17 +234,23 @@ image.onload = () => {
                         }
                     }
                 })) {
-                    // console.log("collision")
                     moving = false;
                     collisionsound.play()
                     break;
                 }
             }
-            player.setAnimation(upImages.slice(upCounter,upImages.length));
-            upCounter=(upCounter+1)%upImages.length;
+            if(isCar){
+                car.setAnimation(upCarImages.slice(upCounter,upCarImages.length));
+                upCarCounter=(upCarCounter+1)%upCarImages.length;
+            }
+            else{
+                player.setAnimation(upImages.slice(upCounter,upImages.length));
+                upCounter=(upCounter+1)%upImages.length;
+            }
+           
             if (moving) {
-                if (isSpeedBoostActive) {
-                    background.position.y += 4 * speedMultiplier; // Increase the speed
+                if (isSpeedBoostActive || isCar) {
+                    background.position.y += 4 * speedMultiplier;
                     foreground.position.y += 4 * speedMultiplier;
                     Boundaries.forEach(boundary => {
                         boundary.position.y += 4 * speedMultiplier;
@@ -220,7 +263,6 @@ image.onload = () => {
                     boundary.position.y += 2;
                 });
                     }
-                
             }
         } else if (keys.a.pressed && lastKey === 'a') {
             for (let i = 0; i < Boundaries.length; i++) {
@@ -233,17 +275,23 @@ image.onload = () => {
                         }
                     }
                 })) {
-                    // console.log("collision")
                     moving = false;
                     collisionsound.play()
                     break;
                 }
             }
-            player.setAnimation(leftImages.slice(leftCounter,leftImages.length));
+            if(isCar){
+                car.setAnimation(leftCarImages.slice(leftCounter,leftCarImages.length));
+                leftCarCounter=(leftCarCounter+1)%leftCarImages.length;
+            }
+            else{
+                player.setAnimation(leftImages.slice(leftCounter,leftImages.length));
             leftCounter=(leftCounter+1)%leftImages.length;
+            }
+            
             if (moving) {
-                if(isSpeedBoostActive){
-                    background.position.x += 4 * speedMultiplier; // Increase the speed
+                if(isSpeedBoostActive || isCar){
+                    background.position.x += 4 * speedMultiplier; 
                     foreground.position.x += 4 * speedMultiplier;
                     Boundaries.forEach(boundary => {
                         boundary.position.x += 4 * speedMultiplier;
@@ -266,17 +314,23 @@ image.onload = () => {
                         }
                     }
                 })) {
-                    // console.log("collision")
                     moving = false;
                     collisionsound.play()
                     break;
                 }
             }
-            player.setAnimation(downImages.slice(downCounter,downImages.length));
-            downCounter=(downCounter+1)%downImages.length;
+            if(isCar){
+                car.setAnimation(downCarImages.slice(downCounter,downCarImages.length));
+                downCarCounter=(downCarCounter+1)%downCarImages.length;
+            }
+            else{
+                player.setAnimation(downImages.slice(downCounter,downImages.length));
+                downCounter=(downCounter+1)%downImages.length;
+            }
+
             if (moving) {
-                if(isSpeedBoostActive){
-                    background.position.y -= 4 * speedMultiplier; // Increase the speed
+                if(isSpeedBoostActive || isCar){
+                    background.position.y -= 4 * speedMultiplier;
                     foreground.position.y -= 4 * speedMultiplier;
                     Boundaries.forEach(boundary => {
                         boundary.position.y -= 4 * speedMultiplier;
@@ -299,17 +353,22 @@ image.onload = () => {
                         }
                     }
                 })) {
-                    // console.log("collision")
                     moving = false;
                     collisionsound.play()
                     break;
                 }
             }
-            player.setAnimation(rightImages.slice(rightCounter,rightImages.length));
-            rightCounter=(rightCounter+1)%rightImages.length;
+            if(isCar){
+                car.setAnimation(rightCarImages.slice(rightCounter,rightCarImages.length));
+                rightCarCounter=(rightCarCounter+1)%rightCarImages.length;
+            }
+            else{
+                player.setAnimation(rightImages.slice(rightCounter,rightImages.length));
+                rightCounter=(rightCounter+1)%rightImages.length;
+            }
             if (moving) {
-                if(isSpeedBoostActive){
-                    background.position.x -= 4 * speedMultiplier; // Increase the speed
+                if(isSpeedBoostActive || isCar){
+                    background.position.x -= 4 * speedMultiplier; 
                     foreground.position.x -= 4 * speedMultiplier;
                     Boundaries.forEach(boundary => {
                         boundary.position.x -= 4 * speedMultiplier;
@@ -343,10 +402,11 @@ const keys = {
     },
     u:{
         pressed:false
-    }
-    
+    },
+    r:{
+        pressed:false
+    }   
 }
-
 window.addEventListener('keydown', (e) => {
 
     switch (e.key) {
@@ -379,6 +439,9 @@ window.addEventListener('keydown', (e) => {
         case 'u':
             isSpeedBoostActive=!isSpeedBoostActive;
             break;
+        case 'r':
+            isCar=!isCar;
+            break;
     }
 })
 window.addEventListener('keyup', (e) => {
@@ -386,27 +449,30 @@ window.addEventListener('keyup', (e) => {
         case 'w':
             keys.w.pressed = false;
             walkingSound.pause()
-            player.setAnimation(upImages.slice(0, 1)); 
+            if(isCar) car.setAnimation(upCarImages.slice(0, 1));
+            else player.setAnimation(upImages.slice(0, 1)); 
             break;
         case 's':
             keys.s.pressed = false;
             walkingSound.pause()
-            player.setAnimation(downImages.slice(0, 1)); 
+            if(isCar) car.setAnimation(downCarImages.slice(0, 1));
+           else  player.setAnimation(downImages.slice(0, 1)); 
             break;
         case 'a':
             keys.a.pressed = false;
             walkingSound.pause()
-            player.setAnimation(leftImages.slice(0, 1)); 
+            if(isCar) car.setAnimation(leftCarImages.slice(0, 1));
+            else player.setAnimation(leftImages.slice(0, 1));
+            
             break;
         case 'd':
             keys.d.pressed = false;
             walkingSound.pause()
-            player.setAnimation(rightImages.slice(0, 1));
+            if(isCar) car.setAnimation(rightCarImages.slice(0, 1));
+           else  player.setAnimation(rightImages.slice(0, 1));
             break;
     }
 });
-
-
 const audioFiles = [
     '../assets/audio/1.mp3',
     '../assets/audio/2.mp3' 
@@ -432,7 +498,6 @@ function toggleMusic() {
     } else {
         bgMusic.volume=0.3
         bgMusic.play();
-        
         isMusicPlaying = true;
     }
 }
